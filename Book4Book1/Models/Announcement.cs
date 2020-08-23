2f85e49079e69ada260ecf210ce95ea6034033b3
Dominik Toczek <CRPW73@motorolasolutions.com>
2020-08-23 17:41:27 +0200

    Add models

---
+++ Book4Book1/Models/Announcement.cs
@@ -0,0 +1,15 @@
+﻿namespace Book4Book1.Models
+{
+    public class Announcement
+    {
+        public int AnnouncementId { get; set; }
+        public int BookId { get; }
+        public int UserId { get; }
+
+        public Announcement(int bookId, int userId)
+        {
+            BookId = bookId;
+            UserId = userId;
+        }
+    }
+}
